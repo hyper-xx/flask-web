378a2e659541959f174e3682b796a29cae7a019d
xuzx_mac <copy_xx@aliyun.com>
2017-04-24 00:55:10 +0800

    sqlite

--- flask-web.py
+++ flask-web.py
@@ -1,7 +1,66 @@
-from flask import Flask,url_for,render_template
+from flask import Flask,url_for,render_template,session,g,redirect,abort,flash
 from flask import request,make_response
+import os
+import sqlite3
+
 
 app = Flask(__name__)
+
+app.config['DATABASE']='/Users/xuzhengxi/PycharmProjects/flask-web/flask.db'
+
+##DB
+#connect DB
+def connect_db():
+    rv=sqlite3.connect('/Users/xuzhengxi/PycharmProjects/flask-web/flask.db')
+    rv.row_factory=sqlite3.Row
+    return rv
+
+#init DB
+def init_db():
+    with app.app_context():
+        db=connect_db()
+        with app.open_resource('schema.sql','r') as f:
+            g.db.cursor().executescript(f.read())
+        db.commit()
+
+##show entrites
+@app.route('/showen/')
+def show_entries():
+    cur=g.db.execute('select title, text from entries order by id desc')
+    entries=[dict(title=row[0],text=row[1]) for row in cur.fetchall()]
+    return render_template('showen.html',entries=entries)
+
+##add entrites
+@app.route('/adden/')
+def add_entry():
+    if not session.get('logged_in'):
+        abort(401)
+    g.db.execute('insert into entries (title, text) values (?, ?)',[request.form['title'],request.form['text']])
+    g.db.commit()
+    flash('New entry was successfully posted')
+    return redirect(url_for('show_entries'))
+
+##login
+@app.route('/login/',methods=['GET','POST'])
+def login():
+    error=None
+    if request.method=='POST':
+        if request.form['username']!=app.config['USRENAME']:
+            error='Invalid username'
+        elif request.form['password']!=app.config['PASSWORD']:
+            error='Invalid password'
+        else:
+            session['loggen_in']=True
+            flash('You were logged in')
+            return redirect(url_for('show_entries'))
+        return render_template('login.html',error=error)
+
+##logout
+@app.route('/logout/')
+def logout():
+    session.pop('logged_in',None)
+    flash('You were logged out')
+    return redirect(url_for('show_entries'))
 
 ##router
 @app.route('/')
